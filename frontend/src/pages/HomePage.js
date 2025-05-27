1907a607fe53c2becb4dd6b0471a5a196d1f798a
Jay Mangat <jaysinghmangat@gmail.com>
2025-05-26 17:40:56 -0700

    improved blobs

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,26 @@
-import React from 'react';
 import LiquidBackground from '../components/LiquidBackground';
+import { Box, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-const HomePage = () => (
-  <LiquidBackground />
-);
+function HomePage() {
+  const theme = useTheme();
+  return (
+    <>
+      <LiquidBackground /> {/* Background fixed behind everything */}
+      <Box sx={{
+        position: 'relative',
+        zIndex: 1, // Ensure content is above background
+        minHeight: '100vh', // Ensure content fills viewport
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Typography variant="h1" color='theme.palette.text.primary'>
+          This is the Home Page
+        </Typography>
+      </Box>
+    </>
+  );
+}
 
 export default HomePage;
