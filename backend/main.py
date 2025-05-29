2d85c6ae33880ea566475fedf1ae8e81c963b7aa
Jay Mangat <jaysinghmangat@gmail.com>
2025-05-28 20:17:45 -0700

    changed fetch URLs to work with vercel/render

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,14 @@ from typing import List
 
 app = FastAPI()
 
+origins = [
+    "https://jay-mangat.vercel.app",
+    "http://localhost:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=origins,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
