e14c0f6d9732023fd5be6ae4821c3b4a25fa747d
alto10002 <jaysinghmangat@gmail.com>
2025-09-25 21:49:58 -0700

    optimize - changed match criteria to include substings (tomato == diced tomato)

--- backend/app/services/generate.py
+++ backend/app/services/generate.py
@@ -9,14 +9,6 @@ load_dotenv(dotenv_path=Path(__file__).resolve().parents[2] / ".env")
 
 FILE = Path(__file__).resolve().parents[2] / "data" / "zstd.parquet"
 all_recipes = pd.read_parquet(FILE, engine="pyarrow")
-# Make ingredients lowercase to assist matching with ingredient list
-all_recipes["ingredients"] = all_recipes["ingredients"].map(
-    lambda ingr: set(map(str.lower, ingr))
-)
-# Force steps to list for ease with JSON
-all_recipes["steps"] = all_recipes["steps"].apply(
-    lambda x: x.tolist() if hasattr(x, "tolist") else x
-)
 
 
 # @profile
@@ -27,16 +19,10 @@ def generate_recipes(user_ingredients):
         )
     ]
 
-    recipe_count = len(filtered_recipes)
-    filtered_recipes_sample = filtered_recipes.sample(3)
-    # Capitalization ingredients so it looks better on cards
-    filtered_recipes_sample["ingredients"] = filtered_recipes_sample[
-        "ingredients"
-    ].apply(lambda s: [i.capitalize() for i in s])
-    return [filtered_recipes_sample, recipe_count]
+    return filtered_recipes
 
 
-# helper for fuzzy matching so tomato returns tomatos/cherry tomato/etc.
+# Matches user's 'tomato' to recipe's 'diced tomato' resulting in more recipes
 def ingredient_match(recipe_ingredients, user_ingredients):
     return all(
         any(ui in ingr for ingr in recipe_ingredients) for ui in user_ingredients
@@ -72,16 +58,47 @@ def add_google_links(recipe_name):
     return image_url, page_url
 
 
-def google_links_wrapper(filtered_recipes_sample):
+def google_links_wrapper(filtered_recipes):
+    filtered_recipes_sample = filtered_recipes.sample(3)
+    filtered_recipes_sample["ingredients"] = filtered_recipes_sample[
+        "ingredients"
+    ].apply(lambda s: [i.capitalize() for i in s])
     filtered_recipes_sample[["image_url", "page_url"]] = filtered_recipes_sample[
         "name"
     ].apply(lambda name: pd.Series(add_google_links(name)))
     return filtered_recipes_sample.to_dict(orient="records")
 
 
-# Testing generate timings/different ingredient combinations
-# generated = generate_recipes({"tomato", "chicken", "garlic", "cheese", "pasta"})
-# print(generated[0])
-# print(generated[1])
-# links = google_links_wrapper(generated[0])
+# # Testing generate timings/different ingredient combinations
+# t1 = time.perf_counter()
+# generated = generate_recipes({"tomato"})
+# t2 = time.perf_counter()
+# print(f"{len(generated)} {t2-t1}")
+
+# t1 = time.perf_counter()
+# generated = generate_recipes({"chicken"})
+# t2 = time.perf_counter()
+# print(f"{len(generated)} {t2-t1}")
+
+# t1 = time.perf_counter()
+# generated = generate_recipes({"garlic"})
+# t2 = time.perf_counter()
+# print(f"{len(generated)} {t2-t1}")
+
+# t1 = time.perf_counter()
+# generated = generate_recipes({"cheese"})
+# t2 = time.perf_counter()
+# print(f"{len(generated)} {t2-t1}")
+
+# t1 = time.perf_counter()
+# generated = generate_recipes({"rice"})
+# t2 = time.perf_counter()
+# print(f"{len(generated)} {t2-t1}")
+
+# t1 = time.perf_counter()
+# generated = generate_recipes({"tomato", "chicken", "garlic", "cheese"})
+# t2 = time.perf_counter()
+# print(f"{len(generated)} {t2-t1}")
+
+# links = google_links_wrapper(generated)
 # print(links)
